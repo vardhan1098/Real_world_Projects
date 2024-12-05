4f5caa3d3b002f0e5a248a85308220027231d087
vardhan1098 <58623502+vardhan1098@users.noreply.github.com>
2024-12-05 11:34:41 +0530

    changes

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnurBfQiyY0BNzuJaVG15BaDZmcp1XvK8",
+  authDomain: "auth-4f6c3.firebaseapp.com",
+  projectId: "auth-4f6c3",
+  storageBucket: "auth-4f6c3.firebasestorage.app",
+  messagingSenderId: "115652694233",
+  appId: "1:115652694233:web:37d61293960f4da36eaec8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/firebase/NewApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewApp() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default NewApp
