c6f0fabd90b0024b10b2749dc8d35f928bc4b3f5
vardhan1098 <58623502+vardhan1098@users.noreply.github.com>
2025-01-08 10:09:46 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef(null);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(intervalRef.current);
+    }
+    return () => clearInterval(intervalRef.current);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
+
+  const formattedTime = () => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    const milliseconds = Math.floor((time % 1000) / 10);
+    return `${minutes.toFixed().padStart(2, "0")} : ${seconds
+      .toFixed()
+      .padStart(2, "0")}: ${milliseconds.toFixed().padStart(2, "0")}`;
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h2>{formattedTime()}</h2>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 };
